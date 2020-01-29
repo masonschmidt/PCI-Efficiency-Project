e6374a8cd34a0963f543f7df956d7787521c0364
masonschmidt <masonschmidt0@ou.edu>
2020-01-28 20:22:19 -0600

    Added fuel requests and (maybe?) fixed timeout errors.

--- mason-python-testing/asycRequests.py
+++ mason-python-testing/asycRequests.py
@@ -7,17 +7,24 @@ generator_data = []
 async def get(url, session, gen_num):
     async with session.get(url) as response:
         async for data, _ in response.content.iter_chunks():
-            generator_data.append(data)
+            #generator_data.append(data)
             if(gen_num == 3000):
-                print(len(generator_data))
+                #print("length: {}, url: {}".format(len(generator_data), url))
+                print(url)
         return response
 
 loop = asyncio.ProactorEventLoop()
 asyncio.set_event_loop(loop)
 
 conn = aiohttp.TCPConnector(limit=0)
-session = aiohttp.ClientSession(connector=conn)
+timeout = aiohttp.ClientTimeout(total=0)
+session = aiohttp.ClientSession(connector=conn, timeout=timeout)
 
-coroutines = [get("http://127.0.0.1:3001/generator/{}/powerProduced".format(i), session, i) for i in range(1, 3001)]
+coroutines = []
+for i in range(1, 3001):
+    coroutines.append(get("http://127.0.0.1:3001/generator/{}/powerProduced".format(i), session, i))
+    coroutines.append(get("http://127.0.0.1:3001/generator/{}/fuelConsumed".format(i), session, i))
+
+#coroutines = [get("http://127.0.0.1:3001/generator/{}/powerProduced".format(i), session, i) for i in range(1, 3001)]
 
 results = loop.run_until_complete(asyncio.gather(*coroutines))
