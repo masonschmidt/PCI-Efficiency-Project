7f9243cc439267d30b59037e6594b57ec5095fae
masonschmidt <masonschmidt0@ou.edu>
2020-01-30 13:09:25 -0600

    Small debug text change and moved s3 resource creation out of async loop

--- mason-python-testing/asyncRequests.py
+++ mason-python-testing/asyncRequests.py
@@ -17,7 +17,7 @@ NUMBER_OF_GENERATORS = 2
 generator_data = dict()
 
 #Open a connection with the generator server asynchonously
-async def get(url, session, gen_num, data_type):
+async def get(url, session, gen_num, data_type, s3):
     #Actual request to the endpoint
     async with session.get(url) as response:
         #Iterate over the data (including when new data arrives)
@@ -36,36 +36,22 @@ async def get(url, session, gen_num, data_type):
                 print("Sending power data for generator {}...".format(gen_num))
                 json_file = json.dumps(generator_data[url])
 
-                #async with session.put("{}.{}/generator{}/{}.json".format(POWER_BUCKET, AWS_PATH, gen_num, json_content['time']),
-                    #data=json_file, auth=AWS_AUTH)
-                s3 = boto3.resource( 's3',
-                    aws_access_key_id=BUCKET_ACCESS_KEY,
-                    aws_secret_access_key=BUCKET_SECRET_ACCESS_KEY,
-                    config=Config(signature_version='s3v4')
-                )
+                s3.Bucket(POWER_BUCKET).put_object(Key='generator{}/{}.json'.format(gen_num,json_content['time']), Body=json_file)
 
-                s3.Bucket(POWER_BUCKET).put_object(Key='{}.json'.format(json_content['time']), Body=json_file)
-                print("Power data sent")
+                print("Power data sent for generator {}".format(gen_num))
+
+                generator_data[url] = []
 
             elif(data_type == 'fuel' and len(generator_data[url]) >= 12):
                 print("Sending fuel data for generator {}...".format(gen_num))
                 json_file = json.dumps(generator_data[url])
 
-                #async with session.put("{}.{}/generator{}/{}.json".format(FUEL_BUCKET, AWS_PATH, gen_num, json_content['time']),
-                    #data=json_file, auth=AWS_AUTH)
-                s3 = boto3.resource( 's3',
-                    aws_access_key_id=BUCKET_ACCESS_KEY,
-                    aws_secret_access_key=BUCKET_SECRET_ACCESS_KEY,
-                    config=Config(signature_version='s3v4')
-                )
+                s3.Bucket(FUEL_BUCKET).put_object(Key='generator{}/{}.json'.format(gen_num,json_content['time']), Body=json_file)
 
-                s3.Bucket(FUEL_BUCKET).put_object(Key='{}.json'.format(json_content['time'], Body=json_file))
-                print("Fuel data sent")
+                print("Fuel data sent for generator {}".format(gen_num))
 
-            #For debugging
-            if(gen_num == NUMBER_OF_GENERATORS):
-                #print("length: {}, url: {}".format(len(generator_data), url))
-                print(url)
+                generator_data[url] = []
+
         return response
 
 #Create a loop for the asyncio library (this one is specific for windows
@@ -80,13 +66,22 @@ timeout = aiohttp.ClientTimeout(total=0)
 #Create a session for our connections with the connector and timeout
 session = aiohttp.ClientSession(connector=conn, timeout=timeout)
 
+s3 = boto3.resource( 's3',
+    aws_access_key_id=BUCKET_ACCESS_KEY,
+    aws_secret_access_key=BUCKET_SECRET_ACCESS_KEY,
+    config=Config(signature_version='s3v4')
+)
+
+#result = s3.get_bucket_acl(Bucket=POWER_BUCKET)
+#print(result)
+
 #Create the coroutines to be run and add them to a list
 coroutines = []
 for i in range(1, NUMBER_OF_GENERATORS+1):
     power_url = "http://127.0.0.1:3001/generator/{}/powerProduced".format(i)
     fuel_url = "http://127.0.0.1:3001/generator/{}/fuelConsumed".format(i)
-    coroutines.append(get(power_url, session, i, 'power'))
-    coroutines.append(get(fuel_url, session, i, 'fuel'))
+    coroutines.append(get(power_url, session, i, 'power', s3))
+    coroutines.append(get(fuel_url, session, i, 'fuel', s3))
 
 #run all the coroutines
 results = loop.run_until_complete(asyncio.gather(*coroutines))
