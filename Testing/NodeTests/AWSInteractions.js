d4b15429f4125a06768552f56f6150ec3f27539d
Mason Schmidt <masonschmidt0@ou.edu>
2020-02-17 17:30:53 -0600

    Testing Some s3 get calls in Testing/NodeTests/AWSInteractions.js

--- Testing/NodeTests/AWSInteractions.js
+++ Testing/NodeTests/AWSInteractions.js
@@ -6,10 +6,12 @@ s3 = new AWS.S3({apiVersion: '2006-03-01'});
 // Create the parameters for calling listObjects
 var bucketParams = {
   Bucket : 'pci-effciency-project-test',
+  StartAfter : 'generator0001/2020-02-17 22:55.json',
+  Prefix : 'generator0001',
 };
 
 // Call S3 to obtain a list of the objects in the bucket
-s3.listObjects(bucketParams, function(err, data) {
+s3.listObjectsV2(bucketParams, function(err, data) {
   if (err) {
     console.log("Error", err);
   } else {
