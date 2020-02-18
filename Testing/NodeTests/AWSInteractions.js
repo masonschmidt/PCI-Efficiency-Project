a6b290d8a09dc5e2aa6b7c61a5fc93046caa7f11
masonschmidt <masonschmidt0@ou.edu>
2020-02-18 00:26:33 -0600

    More messing with get requests

--- Testing/NodeTests/AWSInteractions.js
+++ Testing/NodeTests/AWSInteractions.js
@@ -6,7 +6,7 @@ s3 = new AWS.S3({apiVersion: '2006-03-01'});
 // Create the parameters for calling listObjects
 var bucketParams = {
   Bucket : 'pci-effciency-project-test',
-  StartAfter : 'generator0001/2020-02-17 22:55.json',
+  StartAfter : 'generator0001/2020-02-17 23:03.json',
   Prefix : 'generator0001',
 };
 
@@ -16,5 +16,16 @@ s3.listObjectsV2(bucketParams, function(err, data) {
     console.log("Error", err);
   } else {
     console.log("Success", data);
+    let keyToGet = data['Contents'][0]['Key'];
+
+    var params = {
+      Bucket: 'pci-effciency-project-test',
+      Key: keyToGet,
+    };
+
+    s3.getObject(params, function(err, objectData) {
+       if (err) console.log(err, err.stack); // an error occurred
+       else     console.log(objectData.Body.toString('ascii'));           // successful response
+     });
   }
 });
