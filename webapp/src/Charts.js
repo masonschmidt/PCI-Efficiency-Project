663a7d425272581a1dc5b00502456c13cb9f5327
masonschmidt <masonschmidt0@ou.edu>
2020-04-02 02:19:37 -0500

    Dynamic Chart Sizing

--- webapp/src/Charts.js
+++ webapp/src/Charts.js
@@ -3,21 +3,30 @@ import './App.css';
 import Chart from './Chart.js'
 
 class Charts extends Component {
-  renderChart(id) {
-    return (
-      <Chart
-        id={id}
-      />
-    );
+  renderChart(row, numRows, numColumns) {
+    let items = []
+    for(var j = 0; j < numColumns; j++) {
+      items.push(
+        <Chart
+          id={(row-1)*numColumns + 1 + j}
+          numRows={numRows}
+          numColumns={numColumns}
+          key={(row-1)*numColumns + 1 + j}
+        />
+      )
+    }
+    return items;
   }
 
   renderCharts(numCharts)
   {
     let items = []
-    for(var i = 1; i <= this.props.numCharts; i++){
+    let numRows = Math.ceil(Math.sqrt(numCharts));
+    let numColumns = Math.floor(Math.log2(numCharts));
+    for(var i = 1; i <= numRows; i++){
       items.push(
-        <div id={i} key={i}>
-          {this.renderChart(i)}
+        <div id={i} key={i} className='chartRow'>
+          {this.renderChart(i, numRows, numColumns)}
         </div>
       );
     }
